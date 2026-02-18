867783e86a6701b6259ba18d3229edc2606b771d
Mallela Sathya Prajith Reddy <sathyaprajith5929@gmail.com>
2026-02-18 16:00:22 +0530

    Add solution for pearson-correlation

---
+++ pearson-correlation/pearson-correlation.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def pearson_correlation(X):
+    """
+    Compute Pearson correlation matrix from dataset X.
+    Assumes X is (N samples, M features).
+    """
+    X_centered = X - np.mean(X, axis=0)
+    covariance = np.cov(X, rowvar=False)
+    std_dev = np.std(X, axis=0)
+    return np.corrcoef(X, rowvar=False)
