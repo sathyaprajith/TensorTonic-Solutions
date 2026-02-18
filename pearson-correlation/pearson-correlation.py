fffeeb32eb584b8c99cbbe578486fcbd87c77e77
Mallela Sathya Prajith Reddy <sathyaprajith5929@gmail.com>
2026-02-18 16:00:40 +0530

    Update solution for pearson-correlation

--- pearson-correlation/pearson-correlation.py
+++ pearson-correlation/pearson-correlation.py
@@ -5,7 +5,7 @@ def pearson_correlation(X):
     Compute Pearson correlation matrix from dataset X.
     Assumes X is (N samples, M features).
     """
-    X_centered = X - np.mean(X, axis=0)
-    covariance = np.cov(X, rowvar=False)
-    std_dev = np.std(X, axis=0)
+    # X_centered = X - np.mean(X, axis=0)
+    # covariance = np.cov(X, rowvar=False)
+    # std_dev = np.std(X, axis=0)
     return np.corrcoef(X, rowvar=False)
